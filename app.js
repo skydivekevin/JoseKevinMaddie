76e17787d8b4b342fb3b559e740c38405cbeef48
Kevin Potts <fancydude@Kevins-MacBook-Pro-2.local>
2018-09-06 09:44:33 -0600

    adding JS

---
+++ app.js
@@ -0,0 +1,7 @@
+let jose = document.querySelector('#jose')
+let kevin = document.querySelector('#kevin')
+let maddie = document.querySelector('#maddie')
+
+jose.innerHTML = 'Jose is awesome'
+kevin.innerHTML = 'Kevin is awesomer'
+maddie.innerHTML = 'Maddie is the awesomest'
